6e96bc2b1ddb29301b63040a94aee0e39a5daa25
kdh95 <ehgks3323@gmail.com>
2025-05-27 12:26:07 +0900

    updata

--- front-end/run-with-me/src/components/layout/Footer.tsx
+++ front-end/run-with-me/src/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
-      <p>&copy; {currentYear} 러닝메이트 (가칭). All rights reserved.</p>
+      <p>&copy; {currentYear} RunWithMe. All rights reserved.</p>
       {/* <p>
         <Link to="/privacy-policy">개인정보처리방침</Link> | 
         <Link to="/terms-of-service">이용약관</Link>
